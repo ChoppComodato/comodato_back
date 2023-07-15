245f0a7b17b233b88f4818d6a202ae4bc2651395
unknown <jackone.action.software@gmail.com>
2023-07-15 09:18:32 -0300

    testing env vars

--- app/src/main.py
+++ app/src/main.py
@@ -1,7 +1,24 @@
-from fastapi import FastAPI
+from functools import lru_cache
+
+from fastapi import Depends, FastAPI
+from typing_extensions import Annotated
+
+from ..utils import config
 
 app = FastAPI()
 
+
+@lru_cache()
+def get_settings():
+    return config.Settings()
+
+
+@app.get("/info")
+async def info(settings: Annotated[config.Settings, Depends(get_settings)]):
+    return {
+        "database_name": settings.database_name,
+        "database_username": settings.database_username,
+    }
 
 @app.get("/")
 async def root():
