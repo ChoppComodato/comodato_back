b4ad0c0457fc6c23aa61646803b32e0cae915a07
jackonedev <113382260+jackonedev@users.noreply.github.com>
2023-07-15 16:36:22 -0300

    Merge Develop into main (#12)
    * fixed: replacing double primary key in clientes table for unique contraint in dni variable
    
    * updated gitignore
    
    * added example Dotenv file
    
    * added ORM models: Comodato, Recibo
    
    * updated format
    
    * Delete .github directory

--- app/database/database.py
+++ app/database/database.py
@@ -8,6 +8,7 @@ from ..utils import config
 def get_settings():
     return config.Settings()
 
+
 settings = get_settings()
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
--- app/database/models.py
+++ app/database/models.py
@@ -6,11 +6,10 @@ from .database import Base
 from typing import List
 
 
-
 class Cliente(Base):
     __tablename__ = "clientes"
     id = Column(Integer, primary_key=True, nullable=False)
-    dni = Column(Integer, primary_key=True, nullable=False)
+    dni = Column(Integer, nullable=False, unique=True)
     nombre = Column(String, nullable=False)
     apellido = Column(String, nullable=False)
     direccion = Column(String, nullable=False)
@@ -20,8 +19,52 @@ class Cliente(Base):
     email = Column(String, nullable=False)
     vehiculo = Column(String, nullable=False)
     patente = Column(String(8), nullable=False)
-    
+
     fecha_cumple = Column(Date, nullable=True)
     fecha_registro = Column(TIMESTAMP(timezone=True),
-                        nullable=False, server_default=text('now()'))
-    
+                            nullable=False, server_default=text('now()'))
+
+
+class Comodato(Base):
+    __tablename__ = "comodatos"
+    id = Column(Integer, primary_key=True, nullable=False)
+    cliente_id = Column(Integer, ForeignKey("clientes.id"))
+    cliente = relationship("Cliente", back_populates="comodatos")
+    # a partir de las 00:00:00 del día actual
+    fecha_comodato = Column(Date, nullable=False)
+    # hasta las 23:59:59 del día pasado mañana
+    fecha_devolucion = Column(Date, nullable=False)
+    fecha_registro = Column(TIMESTAMP(timezone=True),
+                            nullable=False, server_default=text('now()'))
+    estado = Column(Boolean, nullable=False, default=True)
+    barril_7_8_9_litros = Column(Integer, nullable=False, default=0)
+    barril_10_12_litros = Column(Integer, nullable=False, default=0)
+    barril_18_litros = Column(Integer, nullable=False, default=0)
+    barril_25_litros = Column(Integer, nullable=False, default=0)
+    barril_30_litros = Column(Integer, nullable=False, default=0)
+    barril_40_50_litros = Column(Integer, nullable=False, default=0)
+    choppera_sin_barril = Column(Integer, nullable=False, default=0)
+    reductor_presion = Column(Integer, nullable=False, default=0)
+    tubo_CO2 = Column(Integer, nullable=False, default=0)
+    peso_tubo_CO2 = Column(Integer, nullable=False, default=0)
+    valvula_automatica = Column(Integer, nullable=False, default=0)
+    cabezal_10_litros = Column(Integer, nullable=False, default=0)
+    adicionales = Column(String, nullable=True)
+    observaciones = Column(String, nullable=True)
+
+
+Cliente.comodatos = relationship(
+    "Comodato", order_by=Comodato.id, back_populates="cliente")
+
+
+class Recibo(Base):
+    __tablename__ = "recibos"
+    id = Column(Integer, primary_key=True, nullable=False)
+    comodato_id = Column(Integer, ForeignKey("comodatos.id"))
+    comodato = relationship("Comodato", back_populates="recibos")
+    cliente_id = Column(Integer, ForeignKey("clientes.id"))
+    cliente = relationship("Cliente", back_populates="recibos")
+    monto_recibo = Column(Integer, nullable=False)
+    estado = Column(Boolean, nullable=False, default=True)
+    fecha_registro = Column(TIMESTAMP(timezone=True),
+                            nullable=False, server_default=text('now()'))
