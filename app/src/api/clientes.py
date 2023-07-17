c4b2e0211e8536c376c2969647d0696d0b55d741
unknown <jackone.action.software@gmail.com>
2023-07-17 18:44:00 -0300

    CRUD done

--- app/src/api/clientes.py
+++ app/src/api/clientes.py
@@ -1,7 +1,7 @@
 from fastapi import Depends, APIRouter, HTTPException, status
 from typing import List
 from sqlalchemy.orm import Session
-from ..schemas.clientes import ClienteOut, ClienteCreate, ClienteUpdate
+from ..schemas.clientes import Cliente, ClienteOut, ClienteCreate, ClienteUpdate
 from ...database import models
 from ...database.database import get_db
 
@@ -10,6 +10,21 @@ router = APIRouter(
     tags=['Clientes']
 )
 
+
+# Create
+
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=Cliente)
+async def create_cliente(cliente: ClienteCreate, db: Session = Depends(get_db)):
+    cliente_nuevo = models.Cliente(**cliente.model_dump())
+
+    db.add(cliente_nuevo)
+    db.commit()
+    db.refresh(cliente_nuevo)
+    return cliente_nuevo
+
+
+# Read all
+
 @router.get("/", response_model=List[ClienteOut])
 def get_clientes(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
 
@@ -17,12 +32,50 @@ def get_clientes(skip: int = 0, limit: int = 100, db: Session = Depends(get_db))
     return clientes
 
 
-@router.post("/", status_code=status.HTTP_201_CREATED, response_model=ClienteOut)
-async def create_cliente(cliente: ClienteCreate, db: Session = Depends(get_db)):
-    cliente_nuevo = models.Cliente(**cliente.model_dump())
+# Read one
 
-    db.add(cliente_nuevo)
+@router.get("/{cliente_id}", response_model=ClienteOut)
+def read_cliente(cliente_id: int, db: Session = Depends(get_db)):
+    db_cliente = db.query(models.Cliente).filter(
+        models.Cliente.id == cliente_id).first()
+    if not db_cliente:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Cliente not found")
+    # transform db_cliente.fecha_cumple to string
+    db_cliente.fecha_cumple = db_cliente.fecha_cumple.strftime("%d/%m/%Y")
+    return db_cliente
+
+
+# Update
+
+@router.put("/{cliente_id}", response_model=ClienteOut)
+def update_cliente(cliente_id: int, cliente_actualizado: ClienteUpdate, db: Session = Depends(get_db)):
+    cliente = db.query(models.Cliente).filter(
+        models.Cliente.id == cliente_id).first()
+
+    if not cliente:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Cliente {cliente_id} no encontrado")
+
+    for field, value in cliente_actualizado.model_dump(exclude_unset=True).items():
+        setattr(cliente, field, value)
+
     db.commit()
-    db.refresh(cliente_nuevo)
+    db.refresh(cliente)
+    cliente.fecha_cumple = cliente.fecha_cumple.strftime("%d/%m/%Y")
 
-    return cliente_nuevo
+    return cliente
+
+
+# Delete
+
+@router.delete("/{cliente_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_cliente(cliente_id: int, db: Session = Depends(get_db)):
+    db_cliente = db.query(models.Cliente).filter(
+        models.Cliente.id == cliente_id)
+    if not db_cliente.first():
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Cliente not found")
+    db_cliente.delete(synchronize_session=False)
+    db.commit()
+    return None
