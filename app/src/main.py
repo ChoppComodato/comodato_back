b1036d2a3167014d887885f614321f3a29e6f1cd
Agustín F. Stigliano <113382260+jackonedev@users.noreply.github.com>
2023-07-18 13:58:23 -0300

    Update main.py
    erasing old comments

--- app/src/main.py
+++ app/src/main.py
@@ -14,24 +14,6 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-
-# @app.get("/clientes/", response_model=List[ClienteOut])
-# def get_clientes(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-
-#     clientes = db.query(models.Cliente).offset(skip).limit(limit).all()
-#     return clientes
-
-
-# @app.post("/clientes/", status_code=status.HTTP_201_CREATED, response_model=ClienteOut)
-# async def create_cliente(cliente: ClienteCreate, db: Session = Depends(get_db)):
-#     cliente_nuevo = models.Cliente(**cliente.model_dump())
-
-#     db.add(cliente_nuevo)
-#     db.commit()
-#     db.refresh(cliente_nuevo)
-
-#     return cliente_nuevo
-
 app.include_router(clientes.router)
 
 @app.get("/")
