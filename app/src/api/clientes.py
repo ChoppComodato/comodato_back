091640d9442afb1e3aa6827f8f2d9571cfd8f4f0
unknown <jackone.action.software@gmail.com>
2023-07-18 19:39:08 -0300

    added async definition to path operations

--- app/src/api/clientes.py
+++ app/src/api/clientes.py
@@ -26,7 +26,7 @@ async def create_cliente(cliente: ClienteCreate, db: Session = Depends(get_db)):
 # Read all
 
 @router.get("/", response_model=List[ClienteOut])
-def get_clientes(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+async def get_clientes(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
 
     clientes = db.query(models.Cliente).offset(skip).limit(limit).all()
     return clientes
@@ -35,7 +35,7 @@ def get_clientes(skip: int = 0, limit: int = 100, db: Session = Depends(get_db))
 # Read one - TODO: usar el DNI para hacer el search
 
 @router.get("/{cliente_id}", response_model=ClienteOut)
-def read_cliente(cliente_id: int, db: Session = Depends(get_db)):
+async def read_cliente(cliente_id: int, db: Session = Depends(get_db)):
     db_cliente = db.query(models.Cliente).filter(
         models.Cliente.id == cliente_id).first()
     if not db_cliente:
@@ -49,7 +49,7 @@ def read_cliente(cliente_id: int, db: Session = Depends(get_db)):
 # Update
 
 @router.put("/{cliente_id}", response_model=ClienteOut)
-def update_cliente(cliente_id: int, cliente_actualizado: ClienteUpdate, db: Session = Depends(get_db)):
+async def update_cliente(cliente_id: int, cliente_actualizado: ClienteUpdate, db: Session = Depends(get_db)):
     cliente = db.query(models.Cliente).filter(
         models.Cliente.id == cliente_id).first()
 
@@ -70,7 +70,7 @@ def update_cliente(cliente_id: int, cliente_actualizado: ClienteUpdate, db: Sess
 # Delete
 
 @router.delete("/{cliente_id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_cliente(cliente_id: int, db: Session = Depends(get_db)):
+async def delete_cliente(cliente_id: int, db: Session = Depends(get_db)):
     db_cliente = db.query(models.Cliente).filter(
         models.Cliente.id == cliente_id)
     if not db_cliente.first():
