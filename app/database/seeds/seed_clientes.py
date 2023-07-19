1ca52294ab42f35c3ab4dfa3c0ae4966ccc52f20
unknown <jackone.action.software@gmail.com>
2023-07-19 08:13:22 -0300

    Testeado que la validacion del request es correcta - aun no consigo remover status 307

--- app/database/seeds/seed_clientes.py
+++ app/database/seeds/seed_clientes.py
@@ -2,6 +2,8 @@ import csv
 import httpx
 import concurrent.futures
 from itertools import count
+import datetime
+
 
 counter = count(start=1, step=1)
 
@@ -24,28 +26,46 @@ with open("MOCK_DATA_CLIENTES.csv", "r", encoding="utf-8") as csvfile:
 
 
 def main():
+    def modificar_formato_fecha(diccionario):
+        fecha_cumple = diccionario.get('fecha_cumple', None)
+        if fecha_cumple is None:
+            return diccionario
+
+        partes_fecha = fecha_cumple.split('/')
+        if len(partes_fecha) != 3:
+            return diccionario
+
+        mes, dia, anio = partes_fecha
+        nueva_fecha = f"{dia}/{mes}/{anio}"
+        diccionario['fecha_cumple'] = nueva_fecha
+        return diccionario
+
 
     # create a manual instance of the post requests
     for i, row in enumerate(data):
-        print(row)
+        row['dni'] = int(row['dni'])
+        row['fecha_cumple'] = modificar_formato_fecha(row)['fecha_cumple']
+
         response = httpx.post("http://localhost:8000/clientes", json=row)
+
         if i == 1:
             break
 
 
     print("\n\n\tPROGRAMA PRINCIPAL\n")
-    print("atributos de httpx.Response")
-    print(dir(response)[-20:])
-    print("\n")
-    print("objeto httpx.Response.request")
-    print(response.request)
-    print("\n")
-    print("objeto httpx.Response.text")
-    print(response.text)
-    print("\n")
-    print("objeto httpx.Response.status_code")
-    print(response.status_code)
-    print("\n")
+    # print("atributos de httpx.Response")
+    # print(dir(response)[-20:])
+    # print("\n")
+    # print("objeto httpx.Response.request")
+    # print(response.request)
+    # print("\n")
+    # print("objeto httpx.Response.text")
+    # print(response.text)
+    # print("\n")
+    # print("objeto httpx.Response.status_code")
+    # print(response.status_code)
+    # print("\n")
+    print(f"row es {row}")
 
 
 
