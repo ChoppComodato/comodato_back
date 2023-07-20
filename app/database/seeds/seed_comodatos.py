5d7a4e0615bbbd2eef54e0cb0fe43378928fd8d5
unknown <jackone.action.software@gmail.com>
2023-07-20 09:10:38 -0300

    completed the CRUD seed for comodatos

--- app/database/seeds/seed_comodatos.py
+++ app/database/seeds/seed_comodatos.py
@@ -38,26 +38,8 @@ def create_comodato():
 
 
 def update_comodato(id):
+    "Update Comodato: la actualización de las variables se hace por default desde el validador de pydantic"
     # Send a PUT request to the /comodatos path with the JSON data
-    # data = {
-    #     "fecha_devolucion": datetime.datetime.now(),
-    #     "devuelto": True,
-    #     "barril_7_8_9_litros": 1,
-    #     "barril_10_12_litros": 1,
-    #     "barril_18_litros": 1,
-    #     "barril_25_litros": 1,
-    #     "barril_30_litros": 1,
-    #     "barril_40_50_litros": 1,
-    #     "choppera_sin_barril": 1,
-    #     "reductor_presion": 1,
-    #     "tubo_CO2": 1,
-    #     "peso_tubo_CO2": 1,
-    #     "valvula_automatica": 1,
-    #     "cabezal_10_litros": 1,
-    #     "adicionales": "test1",
-    #     "observaciones": "test1000000"
-    # }
-
     response = requests.put(f"http://localhost:8000/comodatos/{id}", json={})
 
     # Check the response status code and content
@@ -69,11 +51,46 @@ def update_comodato(id):
         print(response.json())
 
 
+def delete_comodato(id):
+    # Send a DELETE request to the /comodatos path with the JSON data
+    response = requests.delete(f"http://localhost:8000/comodatos/{id}")
+
+    # Check the response status code and content
+    if response.status_code == 204:
+        print(f"Comodatos deleted successfully!")
+    else:
+        print(f"Error deleting comodatos: {response.status_code}")
+
+def get_comodato(id):
+    # Send a GET request to the /comodatos path with the JSON data
+    response = requests.get(f"http://localhost:8000/comodatos/{id}")
+
+    # Check the response status code and content
+    if response.status_code == 200:
+        print(f"Comodatos retrieved successfully!")
+        print(response.json())
+    else:
+        print(f"Error retrieving comodatos: {response.status_code}")
+
+def read_comodatos():
+    # Send a GET request to the /comodatos path with the JSON data
+    response = requests.get(f"http://localhost:8000/comodatos/")
+
+    # Check the response status code and content
+    if response.status_code == 200:
+        print(f"Comodatos retrieved successfully!")
+        print(response.json())
+    else:
+        print(f"Error retrieving comodatos: {response.status_code}")
 
 
 def main():
     # create_comodato()
-    update_comodato(1)
+    # update_comodato(4)
+    # delete_comodato(2)
+    get_comodato(1)
+    print('='*50)
+    read_comodatos()
 
 if __name__ == '__main__':
     main()
