6d264f6f82626b0888edba63a5f3c6b3b88f9598
unknown <jackone.action.software@gmail.com>
2023-07-20 09:02:49 -0300

    added update_comodato to seed_comodato.py

--- app/database/seeds/seed_comodatos.py
+++ app/database/seeds/seed_comodatos.py
@@ -36,9 +36,44 @@ def create_comodato():
         print(f"Error creating comodatos: {response.status_code}")
 
 
-def main():
-    create_comodato()
 
+def update_comodato(id):
+    # Send a PUT request to the /comodatos path with the JSON data
+    # data = {
+    #     "fecha_devolucion": datetime.datetime.now(),
+    #     "devuelto": True,
+    #     "barril_7_8_9_litros": 1,
+    #     "barril_10_12_litros": 1,
+    #     "barril_18_litros": 1,
+    #     "barril_25_litros": 1,
+    #     "barril_30_litros": 1,
+    #     "barril_40_50_litros": 1,
+    #     "choppera_sin_barril": 1,
+    #     "reductor_presion": 1,
+    #     "tubo_CO2": 1,
+    #     "peso_tubo_CO2": 1,
+    #     "valvula_automatica": 1,
+    #     "cabezal_10_litros": 1,
+    #     "adicionales": "test1",
+    #     "observaciones": "test1000000"
+    # }
+
+    response = requests.put(f"http://localhost:8000/comodatos/{id}", json={})
+
+    # Check the response status code and content
+    if response.status_code == 200:
+        print(f"Comodatos updated successfully!")
+        print(response.json())
+    else:
+        print(f"Error updating comodatos: {response.status_code}")
+        print(response.json())
+
+
+
+
+def main():
+    # create_comodato()
+    update_comodato(1)
 
 if __name__ == '__main__':
     main()
