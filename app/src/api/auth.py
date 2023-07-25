15c370a039d266db46666d1e088155dc7f8e85f9
unknown <jackone.action.software@gmail.com>
2023-07-25 12:07:57 -0300

    added Annotation declaration

--- app/src/api/auth.py
+++ app/src/api/auth.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, status, HTTPException, Response
+from typing_extensions import Annotated
 from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
@@ -16,7 +17,9 @@ router = APIRouter(
 
 
 @router.post('/', response_model=users.Token)
-def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+def login(user_credentials: Annotated[OAuth2PasswordRequestForm, Depends()],
+          db: Annotated[Session, Depends(get_db)]
+          ):
     user = db.query(models.User).filter(
         models.User.email == user_credentials.username).first()
 
